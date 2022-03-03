2635b3bcffe934a3ecc67eebb44856fa0cf50f9b
ernestotarilonte <100875283+ernestotarilonte@users.noreply.github.com>
2022-03-03 23:52:46 +0100

    Update Main.java
    Cambio para prueba

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         CCuenta miCuenta;
         double saldoActual;
+        double saldo2;
 
         miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = miCuenta.estado();
